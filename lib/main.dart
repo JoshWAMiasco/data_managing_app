e8cf790e5d892901513d5110cbb94073a1991fa4
Acwideam <63932555+JoshWAMiasco@users.noreply.github.com>
2024-03-06 23:59:47 +0800

    na

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:data_managing_core/data_managing_core.dart';
 import 'package:flutter/material.dart';
+import 'package:rune/rune.dart';
 
 import 'app.dart';
 
@@ -10,6 +11,8 @@ final core = DataManagingCore();
 void main() async {
   runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    Rune rune = Rune();
+    rune.demo();
     await core.init();
     runApp(const App());
   }, (error, stackTrace) {});
